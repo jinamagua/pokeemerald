4db0e48c112e2b80b73a2fd863677d7bb18cc856
jinamagua <jai46@cornell.edu>
2020-10-29 00:29:29 -0400

    buffed move Flash - 2 stages of accuracy down, accuracy 90

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -2075,13 +2075,13 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
 
     [MOVE_FLASH] =
     {
-        .effect = EFFECT_ACCURACY_DOWN,
+        .effect = EFFECT_ACCURACY_DOWN_2 ,
         .power = 0,
         .type = TYPE_NORMAL,
-        .accuracy = 100,
-        .pp = 20,
+        .accuracy = 90,
+        .pp = 15,
         .secondaryEffectChance = 0,
-        .target = MOVE_TARGET_SELECTED,
+        .target = MOVE_TARGET_BOTH,
         .priority = 0,
         .flags = FLAG_PROTECT_AFFECTED | FLAG_MAGICCOAT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED,
         .split = SPLIT_STATUS,
